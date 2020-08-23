ebcfe70e7be4d374e92fbf83b87acbef1d5f047f
zhxie <472408570@qq.com>
2020-08-24 01:34:47 +0800

    support cors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	"todo_beego/controllers"
 )
 
@@ -10,6 +11,16 @@ func init() {
 }
 
 func main() {
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		//AllowAllOrigins:  true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "content-type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+		AllowCredentials: true,
+		AllowOrigins:     []string{"http://10.*.*.*:*", "http://localhost:*", "http://127.0.0.1:*"},
+	}))
+
 	beego.Router("/", &controllers.MainController{})
 
 	//Task
