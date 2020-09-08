717988a778be772fd106448bff6ea4eda8b25c67
zhxie <472408570@qq.com>
2020-09-09 01:23:37 +0800

    fix issue owing to production env

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/plugins/cors"
+	"github.com/astaxie/beego/context"
+	"net/http"
 	"todo_beego/controllers"
 )
 
@@ -12,20 +13,30 @@ func init() {
 
 func main() {
 
-	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"*"},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
-		AllowCredentials: true,
-		AllowOrigins:     []string{"http://10.*.*.*:*", "http://localhost:*", "http://127.0.0.1:*", "http://localhost:*/*/*"},
-	}))
+	var success = []byte("SUPPORT OPTIONS")
+
+	var corsFunc = func(ctx *context.Context) {
+		origin := ctx.Input.Header("Origin")
+		ctx.Output.Header("Access-Control-Allow-Methods", "OPTIONS,DELETE,POST,GET,PUT,PATCH")
+		ctx.Output.Header("Access-Control-Max-Age", "3600")
+		ctx.Output.Header("Access-Control-Allow-Headers", "X-Custom-Header,accept,Content-Type,Access-Token")
+		ctx.Output.Header("Access-Control-Allow-Credentials", "true")
+		ctx.Output.Header("Access-Control-Allow-Origin", origin)
+		if ctx.Input.Method() == http.MethodOptions {
+			// options请求，返回200
+			ctx.Output.SetStatus(http.StatusOK)
+			_ = ctx.Output.Body(success)
+		}
+	}
+
+	beego.InsertFilter("/*", beego.BeforeRouter, corsFunc)
 
 	beego.Router("/", &controllers.MainController{})
 
 	//Task
-	beego.Router("/task/", &controllers.TaskController{}, "get:ListTasks;post:NewTask")
-	beego.Router("/task/:id:int", &controllers.TaskController{}, "get:GetTask;put:UpdateTask;"+
+
+	beego.Router("/todos/api/task", &controllers.TaskController{}, "get:ListTasks;post:NewTask")
+	beego.Router("/todos/api/task/:id:int", &controllers.TaskController{}, "get:GetTask;put:UpdateTask;"+
 		"delete:DeleteTask")
 
 	beego.Run()
